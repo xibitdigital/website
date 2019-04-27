7cacaec40191ad3820822644b8d93fbbc1a57d3f
Daniele Sangalli <sangallidaniele@gmail.com>
2019-04-27 23:50:41 +0100

    Updates

---
+++ precache-manifest.57e50778d7c1b54a33f8c40ca792f32c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3c54b2399eb3b96b05fa809b3e2d8a",
+    "url": "/xibitdigital/website/index.html"
+  },
+  {
+    "revision": "3e952fd66abfc2b38ed2",
+    "url": "/xibitdigital/website/static/css/main.029d08b1.chunk.css"
+  },
+  {
+    "revision": "6e760c99d3d54b2332da",
+    "url": "/xibitdigital/website/static/js/2.f36c21a1.chunk.js"
+  },
+  {
+    "revision": "3e952fd66abfc2b38ed2",
+    "url": "/xibitdigital/website/static/js/main.f713494e.chunk.js"
+  },
+  {
+    "revision": "8b3cca8e9de739be9965",
+    "url": "/xibitdigital/website/static/js/runtime~main.8ab33e37.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+
+importScripts(
+  "/xibitdigital/website/precache-manifest.57e50778d7c1b54a33f8c40ca792f32c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/xibitdigital/website/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
